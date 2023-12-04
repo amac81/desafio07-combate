2c529729bd6a85e79efcc6efad265fe4dd9d6a50
amac81 <arnaldocanelas@gmail.com>
2023-12-04 15:40:35 +0000

    project created

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+/**
+* Programa...
+* 
+* @author  Arnaldo Canelas
+* @version 1.0
+* @since   2023-12-04 
+*/
+
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		//Objeto Scanner para entrada de dados, a partir de System.in 
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("teste");
+		
+		sc.close();
+	}
+}
